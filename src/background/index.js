0bd50d1c1cba93b4dbfb9882016baea6ccd9575c
Anthony Cossins <anthonyecossins@gmail.com>
2019-08-25 22:13:24 +0100

    Fix icon scaling bug when using multi monitors at different pixel densities (#3)

--- src/background/index.js
+++ src/background/index.js
@@ -1,38 +1,6 @@
-const $canvas = document.createElement('canvas');
-const ctx = $canvas.getContext('2d');
+import createIconRenderer from './icon_renderer';
 
-const WIDTH = 19 * window.devicePixelRatio;
-const HEIGHT = 19 * window.devicePixelRatio;
-
-ctx.canvas.width = WIDTH;
-ctx.canvas.height = HEIGHT;
-
-function iconRenderer() {
-  let lastMatch;
-
-  return () => {
-    const img = new Image();
-    const match = matchMedia('(prefers-color-scheme: dark)').matches;
-
-    if (match === lastMatch) {
-      return;
-    }
-
-    img.src = match ? 'default_icon_dark@2x.png' : 'default_icon@2x.png';
-
-    img.onload = () => {
-      ctx.drawImage(img, 0, 0);
-
-      chrome.browserAction.setIcon({
-        imageData: ctx.getImageData(0, 0, WIDTH, HEIGHT)
-      });
-    };
-
-    lastMatch = match;
-  };
-}
-
-const renderIcon = iconRenderer();
+const renderIcon = createIconRenderer();
 
 setInterval(renderIcon, 1000);
 renderIcon();
