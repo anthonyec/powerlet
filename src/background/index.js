bdb2b5b731b3af455fe5064457b06c8937b0ad4f
Anthony Cossins <anthonyecossins@gmail.com>
2020-01-02 12:11:57 +0000

    Move shortcut title change to background script

--- src/background/index.js
+++ src/background/index.js
@@ -1,6 +1,34 @@
 import createIconRenderer from './icon_renderer';
+import normalizeCommandShortcut from './normalize_command_shortcut';
 
 const renderIcon = createIconRenderer();
 
-setInterval(renderIcon, 1000);
-renderIcon();
+function updateTitleWithShortcut() {
+  chrome.commands.getAll(function(commands) {
+    const normalizedCommands = commands.map((command) => {
+      return Object.assign({}, command, {
+        shortcut: normalizeCommandShortcut(command.shortcut)
+      });
+    });
+
+    console.log('normalizedCommands', normalizedCommands);
+
+    const defaultCommand = normalizedCommands.find((command) => {
+      return (command.name = '_execute_browser_action');
+    });
+
+    if (defaultCommand.shortcut) {
+      chrome.browserAction.setTitle({
+        title: `Powerlets (${defaultCommand.shortcut})`
+      });
+    }
+  });
+}
+
+function update() {
+  updateTitleWithShortcut();
+  renderIcon();
+}
+
+setInterval(update, 1000);
+update();
