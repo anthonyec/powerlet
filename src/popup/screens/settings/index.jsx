b5b1858a51aa938ddf16f8d41a8a5a2df2cb2397
Anthony Cossins <anthonyecossins@gmail.com>
2020-01-03 12:21:38 +0000

    Remove temp settings

--- src/popup/screens/settings/index.jsx
+++ src/popup/screens/settings/index.jsx
@@ -3,16 +3,5 @@ import React from 'react';
 import './settings.css';
 
 export default function SettingsScreen() {
-  return (
-    <div className="settings-screen">
-      Shortcut <input value="⇧⌘K" disabled />
-      <button>Edit</button>
-      <br />
-      <br />
-      Send anonymous analytics <input type="checkbox" checked />
-      <br />
-      <br />
-      Made by <a href="http://anthony.ec">Anthony Cossins</a>
-    </div>
-  );
+  return <div className="settings-screen">Settings go here</div>;
 }
