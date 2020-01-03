26886dbc3cad946ce271ee9a4e088e945254a0bb
Anthony Cossins <anthonyecossins@gmail.com>
2020-01-03 12:21:06 +0000

    Format

--- src/popup/screens/settings/index.jsx
+++ src/popup/screens/settings/index.jsx
@@ -6,19 +6,12 @@ export default function SettingsScreen() {
   return (
     <div className="settings-screen">
       Shortcut <input value="⇧⌘K" disabled />
-      <button>
-        Edit
-      </button>
-
-
+      <button>Edit</button>
       <br />
       <br />
-
       Send anonymous analytics <input type="checkbox" checked />
-
       <br />
       <br />
-
       Made by <a href="http://anthony.ec">Anthony Cossins</a>
     </div>
   );
