0c159677b7eb55630f89219b8939a627f954dd32
Anthony Cossins <anthonyecossins@gmail.com>
2020-01-03 10:56:17 +0000

    Add options page basic styles

--- src/popup/screens/settings/index.jsx
+++ src/popup/screens/settings/index.jsx
@@ -1,11 +1,39 @@
 import React from 'react';
+import { version } from '../../../../package.json';
 
 import './settings.css';
 
 export default function Settings() {
   return (
     <div className="settings">
-      Shortcut <input value="⇧⌘K" disabled />
+      <section className="settings-section">
+        <h2 className="settings-section__title">Shortcut</h2>
+        <div>
+          <input type="input" value="⇧⌘K" />
+          <button>Edit</button>
+        </div>
+      </section>
+
+      <section className="settings-section">
+        <h2 className="settings-section__title">Anonymous analytics</h2>
+        <input type="checkbox" defaultChecked />
+      </section>
+
+      <section className="settings-section">
+        <h2 className="settings-section__title">Privacy policy</h2>
+
+        <a href="https://anthony.ec/">External</a>
+      </section>
+
+      <section className="settings-section">
+        <h2 className="settings-section__title">
+          Made by <a href="https://anthony.ec/">Anthony Cossins</a>
+        </h2>
+
+        <h2 className="settings-section__title">Powerlets {version}</h2>
+      </section>
+
+      {/* Shortcut <input value="⇧⌘K" disabled />
       <button>
         Edit
       </button>
@@ -19,7 +47,7 @@ export default function Settings() {
       <br />
       <br />
 
-      Made by <a href="http://anthony.ec">Anthony Cossins</a>
+      Made by <a href="http://anthony.ec">Anthony Cossins</a> */}
     </div>
   );
 }
