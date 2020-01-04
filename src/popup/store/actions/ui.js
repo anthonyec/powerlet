2de3415b9759a9b241459dd4dae009316a93eaf2
Anthony Cossins <anthonyecossins@gmail.com>
2020-01-04 23:23:58 +0000

    Remove unused feature code

--- src/popup/store/actions/ui.js
+++ src/popup/store/actions/ui.js
@@ -1,31 +1,5 @@
-export const SET_PRESSED_KEYS = 'SET_PRESSED_KEYS';
-export const SET_QUERY = 'SET_QUERY';
-
 export function navigateTo(url) {
   return (dispatch, getState, { browser }) => {
     browser.tabs.create({ url });
-  };
-}
-
-export function setPressedKeys(keys) {
-  return {
-    type: SET_PRESSED_KEYS,
-    payload: keys
-  };
-}
-
-export function fetchPressedKeys() {
-  return (dispatch, getState, { browser }) => {
-    browser.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-      browser.tabs.sendMessage(
-        tabs[0].id,
-        {
-          type: 'GET_PRESSED_KEYS'
-        },
-        (response) => {
-          dispatch(setPressedKeys(response.payload));
-        }
-      );
-    });
   };
 }
