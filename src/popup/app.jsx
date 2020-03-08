09b1533f6a91ce9fd49317da78a8ca04a5479668
Anthony Cossins <anthonyecossins@gmail.com>
2020-03-08 12:55:37 +0000

    Add path params to router

--- src/popup/app.jsx
+++ src/popup/app.jsx
@@ -1,24 +1,40 @@
 import React, { Suspense } from 'react';
 
+import zipObject from '../utils/zipObject';
+
 import HomeScreen from './screens/home_screen';
 const SettingsScreen = React.lazy(() => import('./screens/settings'));
+const EditorScreen = React.lazy(() => import('./screens/editor'));
 
 import './reset.css';
 import './app.css';
 
 export default function App() {
-  const path = window.location.hash.replace('#', '');
+  const path = window.location.hash.replace('#', '').split('/');
+  const base = path[0];
+  const params = path.slice(1);
+
   const screens = {
     home: HomeScreen,
-    settings: SettingsScreen
+    settings: SettingsScreen,
+    editor: EditorScreen
   };
 
-  const Screen = screens[path] ? screens[path] : screens.home;
+  const screenParams = {
+    editor: ['id']
+  };
+
+  const route = {
+    base,
+    params: zipObject(params, screenParams[base])
+  };
+
+  const Screen = screens[base] ? screens[base] : screens.home;
 
   return (
     <div className="app">
       <Suspense fallback={<div>Loading...</div>}>
-        <Screen />
+        <Screen route={route} />
       </Suspense>
     </div>
   );
---
+++ src/utils/zipObject.js
@@ -0,0 +1,11 @@
+export default function zipObject(arrA, arrB) {
+  let props = {};
+
+  // Intentionally not using reduce ;--)
+  arrA.forEach((value, index) => {
+    const key = arrB[index]
+    props[key] = value;
+  });
+
+  return props;
+}
