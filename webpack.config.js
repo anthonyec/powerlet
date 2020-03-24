d2a585033c04c3537575882f53745007877a50f9
Anthony Cossins <anthonyecossins@gmail.com>
2020-03-24 12:00:10 +0000

    Remove source map

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,4 @@
 const path = require('path');
-const { SourceMapDevToolPlugin } = require('webpack');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
@@ -54,7 +53,6 @@ module.exports = {
     new ExtractTextPlugin({
       filename: 'popup.css',
       allChunks: true
-    }),
-    new SourceMapDevToolPlugin({})
+    })
   ]
 };
