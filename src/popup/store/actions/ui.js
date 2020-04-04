5c1fddafe4748d6926470ffa4c3f6ab9b0f2dbbc
Anthony Cossins <anthonyecossins@gmail.com>
2020-04-04 23:35:11 +0100

    Open editor in center of screen

--- src/popup/store/actions/ui.js
+++ src/popup/store/actions/ui.js
@@ -1,3 +1,6 @@
+const EDITOR_WINDOW_WIDTH = 500;
+const EDITOR_WINDOW_HEIGHT = 450;
+
 export function navigateTo(url) {
   return (dispatch, getState, { browser }) => {
     browser.tabs.create({ url });
@@ -7,12 +10,12 @@ export function navigateTo(url) {
 export function openEditorWindow(id = '') {
   return (dispatch, getState, { browser }) => {
     browser.windows.create({
-      width: 500,
-      height: 450,
+      width: EDITOR_WINDOW_WIDTH,
+      height: EDITOR_WINDOW_HEIGHT,
       url: `popup.html#editor/${id}`,
       type: 'popup',
-      top: 100,
-      left: 100
+      left: Math.floor((screen.availWidth / 2) - (EDITOR_WINDOW_WIDTH / 2)),
+      top: Math.floor((screen.availHeight / 2) - (EDITOR_WINDOW_HEIGHT / 2))
     });
   };
 }
