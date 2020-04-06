b235bb61abaa4e29457f569f6d1e158bc7faf738
Anthony Cossins <anthonyecossins@gmail.com>
2020-04-06 21:20:36 +0100

    Add focus style

--- src/popup/components/code_editor/index.jsx
+++ src/popup/components/code_editor/index.jsx
@@ -2,14 +2,14 @@ import React, { useState, useEffect } from 'react';
 import AceEditor from 'react-ace';
 
 import 'ace-builds/webpack-resolver';
-import "ace-builds/src-noconflict/mode-javascript";
-import "ace-builds/src-noconflict/theme-terminal";
-
+import 'ace-builds/src-noconflict/mode-javascript';
+import 'ace-builds/src-noconflict/theme-terminal';
 
 import './code_editor.css';
 
 export default function CodeEditor({ defaultValue, onChange = () => {} }) {
   const [value, setValue] = useState(defaultValue);
+  const [focused, setFocused] = useState(false);
 
   // When `defaultValue` prop changes, change the local state with that value.
   useEffect(() => {
@@ -21,16 +21,31 @@ export default function CodeEditor({ defaultValue, onChange = () => {} }) {
     onChange(value);
   };
 
+  const handleOnFocus = () => {
+    setFocused(true);
+  };
+
+  const handleOnBlur = () => {
+    setFocused(false);
+  };
+
+  const className = focused
+    ? 'code-editor code-editor--focus'
+    : 'code-editor';
+
   return (
-    <div className="code-editor">
+    <div className={className}>
       <AceEditor
         className="code-editor__input"
         value={value}
         mode="javascript"
         theme="terminal"
-        fontSize="14"
+        fontSize={13}
+        wrapEnabled={true}
         tabSize="2"
         onChange={handleOnChange}
+        onFocus={handleOnFocus}
+        onBlur={handleOnBlur}
       />
     </div>
   );
