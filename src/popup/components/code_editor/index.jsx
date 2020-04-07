2cf16f0a1cac696d9b8d0ace9c0f49f87b06328d
Anthony Cossins <anthonyecossins@gmail.com>
2020-04-07 08:28:26 +0100

    Add code editor color scheme support

--- src/popup/components/code_editor/index.jsx
+++ src/popup/components/code_editor/index.jsx
@@ -4,12 +4,32 @@ import AceEditor from 'react-ace';
 import 'ace-builds/webpack-resolver';
 import 'ace-builds/src-noconflict/mode-javascript';
 import 'ace-builds/src-noconflict/theme-terminal';
+import 'ace-builds/src-noconflict/theme-github';
 
 import './code_editor.css';
 
 export default function CodeEditor({ defaultValue, onChange = () => {} }) {
   const [value, setValue] = useState(defaultValue);
   const [focused, setFocused] = useState(false);
+  const [theme, setTheme] = useState('terminal');
+
+  const handleOnColorSchemeChange = (evt) => {
+    const isDarkTheme = evt.matches;
+
+    if (isDarkTheme) {
+      setTheme('terminal');
+    } else {
+      setTheme('github');
+    }
+  };
+
+  useEffect(() => {
+    const schemeMediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
+
+    handleOnColorSchemeChange(schemeMediaQuery);
+
+    schemeMediaQuery.addListener(handleOnColorSchemeChange);
+  }, []);
 
   // When `defaultValue` prop changes, change the local state with that value.
   useEffect(() => {
@@ -39,7 +59,7 @@ export default function CodeEditor({ defaultValue, onChange = () => {} }) {
         className="code-editor__input"
         value={value}
         mode="javascript"
-        theme="terminal"
+        theme={theme}
         fontSize={13}
         wrapEnabled={true}
         tabSize="2"
