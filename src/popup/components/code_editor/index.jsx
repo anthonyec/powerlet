88b68e7d54ff8ea4bf3d21a2a98edac96b9851a0
Anthony Cossins <anthonyecossins@gmail.com>
2020-04-07 21:52:08 +0100

    Change <CodeEditor /> component to use CodeMirror (#39)

--- src/popup/components/code_editor/index.jsx
+++ src/popup/components/code_editor/index.jsx
@@ -1,27 +1,63 @@
 import React, { useState, useEffect } from 'react';
+import { Controlled as CodeMirror } from 'react-codemirror2';
+
+require('codemirror/mode/javascript/javascript');
+require('codemirror/addon/edit/closebrackets');
 
 import './code_editor.css';
+import 'codemirror/lib/codemirror.css';
+import 'codemirror/theme/material.css';
+
+const CODE_MIRROR_OPTIONS = {
+  tabSize: 2,
+  lineNumbers: true,
+  autoCloseBrackets: true,
+  theme: 'default'
+};
 
 export default function CodeEditor({ defaultValue, onChange = () => {} }) {
   const [value, setValue] = useState(defaultValue);
+  const [options, setOptions] = useState(CODE_MIRROR_OPTIONS);
+
+  const handleOnColorSchemeChange = (evt) => {
+    const isDarkTheme = evt.matches;
+    const theme = isDarkTheme ? 'material' : 'default';
+
+    setOptions({
+      ...CODE_MIRROR_OPTIONS,
+      theme
+    });
+  };
+
+  useEffect(() => {
+    const schemeMediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
+
+    handleOnColorSchemeChange(schemeMediaQuery);
+    schemeMediaQuery.addListener(handleOnColorSchemeChange);
+
+    return () => {
+      schemeMediaQuery.removeListener(handleOnColorSchemeChange);
+    }
+  }, []);
 
   // When `defaultValue` prop changes, change the local state with that value.
   useEffect(() => {
     setValue(defaultValue);
   }, [defaultValue]);
 
-  const handleOnChange = (evt) => {
-    setValue(evt.target.value);
-    onChange(evt.target.value);
+  const handleOnChange = (value) => {
+    setValue(value);
+    onChange(value);
   };
 
   return (
     <div className="code-editor">
-      <textarea
-        className="code-editor__input"
-        onChange={handleOnChange}
+      <CodeMirror
         value={value}
-        spellCheck={false}
+        onBeforeChange={(editor, data, value) => {
+          handleOnChange(value);
+        }}
+        options={options}
       />
     </div>
   );
