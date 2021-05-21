e675009d48d3fd69293d2d7cb5e714bf18c7403b
Anthony Cossins <anthonyecossins@gmail.com>
2021-05-21 11:38:13 +0100

    Refactor homescreen to use new list component and scroll view

--- src/popup/screens/home_screen/index.jsx
+++ src/popup/screens/home_screen/index.jsx
@@ -1,19 +1,16 @@
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { useCallbackRef } from 'use-callback-ref';
 
 import {
   fetchAllBookmarklets,
   executeBookmarklet
 } from '../../store/actions/bookmarklets';
-import { openEditorWindow } from '../../store/actions/ui';
-import { createNewBookmarklet } from '../../store/actions/editor';
 
+import useFuzzyFilter from './use_fuzzy_filter';
 import SearchField from '../../components/search_field';
-import SearchList from '../../components/search_list';
 import ScrollView from '../../components/scroll_view';
+import List from '../../components/list';
 import OnboardMessage from '../../components/onboard_message';
-import ItemActions from '../../components/item_actions';
 import Toolbar from '../../components/toolbar';
 import Icon from '../../components/icon';
 import Button from '../../components/button';
@@ -21,158 +18,49 @@ import Button from '../../components/button';
 import './home_screen.css';
 
 const HIDE_EDITOR = true;
-const KEYS = {
-  ENTER: 13,
-  UP: 38,
-  DOWN: 40,
-  RIGHT: 39,
-  LEFT: 37
-};
 
 export default function HomeScreen() {
-  const bookmarklets = useSelector((state) => state.bookmarklets.all);
   const dispatch = useDispatch();
-  const [searchQuery, setSearchQuery] = useState('');
-  const [selectedIndex, setSelectedIndex] = useState(0);
-  const [scrollViewY, setScrollViewY] = useState(0);
-  const [currentScrollViewY, setCurrentScrollViewY] = useState(0);
-  const [totalItems, setTotalItems] = useState(0);
-  const [currentItem, setCurrentItem] = useState();
+  const searchFieldRef = useRef(null);
+  const [listSelectedItemRef, setListSelectedItemRef] = useState(null);
+  const bookmarklets = useSelector((state) => state.bookmarklets.all);
+  const [fuzzyFilterResults, fuzzyFilter] = useFuzzyFilter(bookmarklets);
 
-  const selectedItemTop = selectedIndex * 40;
-  const selectedItemBottom = selectedIndex * 40 + 40;
+  const handleInputChange = (evt) => {
+    const value = evt.currentTarget.value;
+    fuzzyFilter(value);
+  };
 
-  // https://dev.to/thekashey/the-same-useref-but-it-will-callback-8bo
-  const searchInputRef = useCallbackRef(null, (ref) => {
-    ref && ref.focus();
-  });
+  const handleItemClick = (item) => {
+    dispatch(executeBookmarklet(item.id, item.url));
+    window.close();
+  };
 
-  useEffect(() => {
-    dispatch(fetchAllBookmarklets());
+  const onListItemRefChange = useCallback((element) => {
+    // TODO: Why is it null on first render? Because it's mounting?
+    setListSelectedItemRef(element);
   }, []);
 
   useEffect(() => {
-    if (selectedIndex >= 0 && selectedItemTop < currentScrollViewY) {
-      setScrollViewY(selectedItemTop);
-    }
-
-    if (
-      selectedIndex <= totalItems - 1 &&
-      selectedItemBottom > currentScrollViewY + 400
-    ) {
-      setScrollViewY(selectedItemBottom - 400);
-    }
-  }, [selectedIndex, totalItems]);
-
-  useEffect(() => {
-    setSelectedIndex(0);
-  }, [searchQuery]);
-
-  const execute = (id, url) => {
-    dispatch(executeBookmarklet(id, url));
-    window.close();
-  };
-
-  const getPrevIndex = () => {
-    const calculatedIndex = selectedIndex - 1;
-    return calculatedIndex < 0 ? totalItems - 1 : calculatedIndex;
-  };
-
-  const getNextIndex = () => {
-    const calculatedIndex = selectedIndex + 1;
-    return calculatedIndex > totalItems - 1 ? 0 : calculatedIndex;
-  };
-
-  const handleSpecialKey = (evt) => {
-    switch (evt.keyCode) {
-      case KEYS.ENTER:
-        if (currentItem.url) {
-          execute(currentItem.id, currentItem.url);
-        }
-
-        break;
-      case KEYS.UP:
-        setSelectedIndex(getPrevIndex());
-        break;
-
-      case KEYS.DOWN:
-        setSelectedIndex(getNextIndex());
-        break;
-
-      default:
-    }
-  };
-
-  const handleInputChange = (evt) => {
-    if (evt.keyCode) {
-      switch (evt.keyCode) {
-        case KEYS.LEFT:
-        case KEYS.RIGHT:
-          return;
-
-        case KEYS.ENTER:
-        case KEYS.UP:
-        case KEYS.DOWN:
-          evt.preventDefault();
-          handleSpecialKey(evt);
-          return;
-        default:
-      }
-    }
-
-    setSearchQuery(searchInputRef.current.value);
-  };
-
-  const handleBookmarkletClick = (item) => {
-    execute(item.id, item.url);
-  };
-
-  const handleItemSelect = (item, total) => {
-    setCurrentItem(item);
-    setTotalItems(total);
-  };
-
-  const handleOnScroll = (x, y) => {
-    setCurrentScrollViewY(y);
-  };
-
-  const handleOnEditClick = async (id) => {
-    dispatch(openEditorWindow(id));
-    window.close();
-  };
-
-  const handleOnNewClick = () => {
-    dispatch(createNewBookmarklet());
-    window.close();
-  };
+    dispatch(fetchAllBookmarklets());
+    searchFieldRef.current && searchFieldRef.current.focus();
+  }, []);
 
   return (
     <div className="home-screen">
       <SearchField
-        ref={searchInputRef}
-        onKeyDown={handleInputChange}
+        ref={searchFieldRef}
         onChange={handleInputChange}
         placeholder="Search scripts"
       />
 
       {bookmarklets.length !== 0 && (
-        <ScrollView y={scrollViewY} onScroll={handleOnScroll}>
-          <SearchList
-            query={searchQuery}
-            items={bookmarklets}
-            selected={selectedIndex}
-            onChange={handleItemSelect}
-            onItemClick={handleBookmarkletClick}
-            onItemSelect={handleItemSelect}
-            renderItemActions={(item, isSelected, isMouseOver) => {
-              if (!HIDE_EDITOR && (isSelected || isMouseOver)) {
-                return (
-                  <ItemActions
-                    onEditClick={handleOnEditClick.bind(null, item.id)}
-                  />
-                );
-              }
-            }}
+        <ScrollView targetRef={listSelectedItemRef}>
+          <List
+            ref={{ selectedItem: onListItemRefChange }}
+            items={fuzzyFilterResults}
+            onItemClick={handleItemClick}
+            placeholder="Untitled script"
           />
         </ScrollView>
       )}
