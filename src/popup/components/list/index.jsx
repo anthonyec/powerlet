12d8d4f39aeda37c6c7742dc3ebd6e0d0d11a7a2
Anthony Cossins <anthonyecossins@gmail.com>
2021-05-21 09:21:32 +0100

    Add new list component and move fuzzy filter into hook

---
+++ src/popup/components/list/index.jsx
@@ -0,0 +1,98 @@
+import React, { useEffect, useState } from 'react';
+
+import './list.css';
+
+const KEYS = {
+  ENTER: 13,
+  UP: 38,
+  DOWN: 40,
+  RIGHT: 39,
+  LEFT: 37
+};
+
+// Return an array of concatenated ids. This is used for useEffect which only
+// shallow compares. Thus would always render when an array is used instead
+// of a string or number.
+// E.g [{ id: 3 }, { id: 2 }] becomes '32'.
+function getArrayAsStringOfIds(array) {
+  return array.map((item) => item.id).join('');
+}
+
+export default function List({ items, onItemClick = () => {} }) {
+  const [selectedItemIndex, setSelectedItemIndex] = useState(0);
+
+  const handleItemClick = (item) => {
+    onItemClick(item);
+  };
+
+  const handleItemEnter = () => {
+    const item = items[selectedItemIndex];
+    onItemClick(item);
+  };
+
+  const getPrevIndex = () => {
+    const calculatedIndex = selectedItemIndex - 1;
+    return calculatedIndex < 0 ? items.length - 1 : calculatedIndex;
+  };
+
+  const getNextIndex = () => {
+    const calculatedIndex = selectedItemIndex + 1;
+    return calculatedIndex > items.length - 1 ? 0 : calculatedIndex;
+  };
+
+  const handleKeyDown = (evt) => {
+    switch (evt.keyCode) {
+      case KEYS.ENTER:
+        evt.preventDefault();
+        handleItemEnter();
+        break;
+
+      case KEYS.UP:
+        evt.preventDefault();
+        setSelectedItemIndex(getPrevIndex());
+        break;
+
+      case KEYS.DOWN:
+        evt.preventDefault();
+        setSelectedItemIndex(getNextIndex());
+        break;
+
+      default:
+    }
+  };
+
+  useEffect(() => {
+    window.document.addEventListener('keydown', handleKeyDown);
+
+    return () => {
+      window.document.removeEventListener('keydown', handleKeyDown);
+    };
+
+    // TODO: Is adding and removing event listeners this frequently a bad thing?
+  }, [selectedItemIndex, getArrayAsStringOfIds(items)]);
+
+  // Reset selected to first item when `items` change to avoid selected being
+  // out of bounds when the array length changes.
+  useEffect(() => {
+    setSelectedItemIndex(0);
+  }, [getArrayAsStringOfIds(items)]);
+
+  const renderedItems = items.map((item, index) => {
+    const isSelected = index === selectedItemIndex;
+    const className = isSelected
+      ? 'list__item list__item--selected'
+      : 'list__item';
+
+    return (
+      <li
+        key={item.id}
+        className={className}
+        onClick={handleItemClick.bind(null, item)}
+      >
+        <div className="list__text">{item.title || 'Untitled script'}</div>
+      </li>
+    );
+  });
+
+  return <div className="list">{renderedItems}</div>;
+}
