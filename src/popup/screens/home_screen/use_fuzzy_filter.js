20a04c4e35d74ec9a32e6f9019310b6cc0c8950e
Anthony Cossins <anthonyecossins@gmail.com>
2021-05-29 20:06:57 +0100

    Revert "Remove fuse.js and use fuzzyMatch which is similar to sublime text"
    This reverts commit a1a635e73aef80f8c80a612d4ba7a07e3e6e1e6f.

--- src/popup/screens/home_screen/use_fuzzy_filter.js
+++ src/popup/screens/home_screen/use_fuzzy_filter.js
@@ -1,13 +1,29 @@
-import { useState } from 'react';
-import { fuzzyMatchArray } from './fuzzyMatch';
+import { useState, useEffect, useRef } from 'react';
+import Fuse from 'fuse.js';
 
 export default function useFuzzyFilter(items) {
+  const fuseRef = useRef({
+    search: () => {}
+  });
   const [searchTerm, setSearchTerm] = useState('');
+
+  useEffect(() => {
+    fuseRef.current = new Fuse(items, {
+      shouldSort: true,
+      threshold: 0.6,
+      location: 2,
+      distance: 100,
+      maxPatternLength: 32,
+      minMatchCharLength: 0,
+      keys: ['title']
+    });
+  }, [items.length]);
+
   const query = function (searchTerm = '') {
     setSearchTerm(searchTerm);
   };
-  const results =
-    (searchTerm && fuzzyMatchArray(items, 'title', searchTerm)) || items;
+
+  const results = (searchTerm && fuseRef.current.search(searchTerm)) || items;
 
   return [results, query];
 }
--- src/popup/screens/home_screen/use_sort_by_recent.js
+++ src/popup/screens/home_screen/use_sort_by_recent.js
@@ -1,8 +1,5 @@
 export default function useSortByRecent(results = []) {
-  // This avoids mutating the original results array, causing sorting to affect
-  // the fuzzy results outside of this hook.
-  // TODO: Find a way to do this nicely. Immutable library for arrays?
-  return [...results].sort((a, b) => {
+  return results.sort((a, b) => {
     if (
       (a.group === 'recent' && !b.group) ||
       (!a.group && b.group === 'recent')
