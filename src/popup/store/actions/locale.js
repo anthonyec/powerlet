552389bf322dbe4602047c60142577ad64352e86
Anthony Cossins <anthonyecossins@gmail.com>
2021-06-06 17:34:20 +0100

    Remove console log and refind beutiful comment

--- src/popup/store/actions/locale.js
+++ src/popup/store/actions/locale.js
@@ -1,8 +1,6 @@
 export const SET_MESSAGES = 'SET_MESSAGES';
 
 export function setMessages(messages = {}) {
-  console.log('setMessages', messages);
-
   return {
     type: SET_MESSAGES,
     payload: messages
@@ -11,11 +9,11 @@ export function setMessages(messages = {}) {
 
 export function fetchLocaleMessages() {
   return async (dispatch, getState, { browser }) => {
-    // Used to get the locale message keys. It does not use this to get the
-    // actual translations.
-    // TODO: Is this fine? Duplicate data technically. Ideally I'd like to
-    // reference `_locales/` that already exists in dist.
-    // https://webpack.js.org/guides/asset-modules/
+    // Dynamic import is used to get the locale message **keys** and
+    // not the actual translation strings.
+    // TODO: Is this fine? Data is duplicated, ideally I'd like to reference
+    // `_locales/` that already exists in dist. Look into how to do this with
+    // Wwebpack 5: https://webpack.js.org/guides/asset-modules/
     const localeMessages = await import('../../../_locales/en/messages.json');
 
     const messages = Object.keys(localeMessages).reduce(
