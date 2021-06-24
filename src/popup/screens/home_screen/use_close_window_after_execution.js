4806a669595fc4aa277c336f628023b0e1a6609e
Anthony Cossins <anthonyecossins@gmail.com>
2021-06-24 21:06:56 +0100

    Add comment explaining close window hook

--- src/popup/screens/home_screen/use_close_window_after_execution.js
+++ src/popup/screens/home_screen/use_close_window_after_execution.js
@@ -1,7 +1,10 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { selectShouldCloseWindowAfterExecutingScript } from '../../store/selectors/bookmarklets';
 
+// Hook to close the window only after a script has been executed.
+// This is done by checking if the most recent script
+// matches the `setExecutedScriptId`.
 export default function useCloseWindowAfterExecution() {
   const [executedScriptId, setExecutedScriptId] = useState(null);
   const shouldCloseWindow = useSelector(
