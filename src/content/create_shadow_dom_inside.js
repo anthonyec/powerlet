f1600dddc5c47bc4184603c07a9d266b9493089d
Anthony Cossins <1451668+anthonyec@users.noreply.github.com>
2023-04-19 22:25:44 +0800

    Add button styles

--- src/content/create_shadow_dom_inside.js
+++ src/content/create_shadow_dom_inside.js
@@ -1,4 +1,4 @@
-export function createShadowDom() {
+export function createShadowDomInside(target) {
   const shadowElement = document.createElement('div');
 
   shadowElement.style.background = "none";
@@ -7,15 +7,17 @@ export function createShadowDom() {
   shadowElement.style.boxShadow = "none";
   shadowElement.style.margin = "0";
   shadowElement.style.padding = "0";
-  shadowElement.style.position = "absolute";
   shadowElement.style.top = "0";
   shadowElement.style.left = "0";
   shadowElement.style.transform = "none";
   shadowElement.style.zIndex = "2147483647";
-  shadowElement.style.width = "100%";
+  shadowElement.style.width = "0";
   shadowElement.style.height = "0";
+  shadowElement.style.minWidth = "0";
+  shadowElement.style.minHeight = "0";
+  shadowElement.style.display = "inline-block";
 
-  document.body.appendChild(shadowElement);
+  target.appendChild(shadowElement);
 
   return shadowElement.attachShadow({ mode: "open" });
 }
--- src/content/index.js
+++ src/content/index.js
@@ -1,73 +1,73 @@
-import { createShadowDom } from './create_shadow_dom';
+import { createShadowDomInside } from './create_shadow_dom_inside';
 import addIcon from './add_icon.svg';
 
-const ADD_REMOVE_BUTTON_ID = "powerlet-add-remove-button"
+const BUTTON_ID = "powerlet-add-remove-button"
+const LABEL_ID = "powerlet-add-remove-button__label"
 
-const shadow = createShadowDom();
-const scripts = document.querySelectorAll('[href*="javascript:"]');
+const scripts = document.body.querySelectorAll('[href*="javascript:"]');
 
-const style = document.createElement('style');
+Array.from(scripts).forEach((script) => {
+  const shadow = createShadowDomInside(script);
+  const style = document.createElement('style');
 
-style.innerHTML = `
-  #${ADD_REMOVE_BUTTON_ID} {
-    color: white;
-    background-image: linear-gradient(-30deg, #D86299 0%, #A449D6 100%);
-    border-radius: 13.5px;
-    border: none;
-    cursor: pointer;
-    display: flex;
-    align-items: center;
-    padding: 5px 10px;
-    padding-right: 14px;
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-    font-size: 15px;
-    font-weight: 500;
-    position: absolute;
-    top: 10px;
-    left: 10px;
-    gap: 5px;
-  }
-`;
+  const button = document.createElement('button');
+  const icon = document.createElement('img');
+  const label = document.createElement('span');
 
-const button = document.createElement("button");
-const icon = document.createElement('img');
+  icon.src = addIcon;
 
-icon.src = addIcon;
+  button.id = BUTTON_ID;
+  button.style.position = "relative";
 
-button.id = ADD_REMOVE_BUTTON_ID;
-button.textContent = "Add Bookmarklet";
+  label.id = LABEL_ID;
+  label.textContent = "Add Bookmarklet";
 
-button.prepend(icon);
-shadow.appendChild(style);
-shadow.appendChild(button);
+  style.innerHTML = `
+    #${BUTTON_ID} {
+      color: white;
+      background-image: linear-gradient(-30deg, #D86299 0%, #A449D6 100%);
+      border-radius: 13.5px;
+      border: none;
+      cursor: pointer;
+      display: flex;
+      align-items: center;
+      padding: 5px;
+      font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+      font-size: 15px;
+      font-weight: 500;
+      position: absolute;
+      width: 27px;
+      height: 27px;
+      transform: translateY(-50%);
+      transition: all;
+      gap: 5px;
+    }
 
-// Array.from(scripts).forEach((script) => {
-//   const href = script.getAttribute('href');
+    #${BUTTON_ID} img {
+      flex-shrink: 0;
+    }
 
-//   script.addEventListener('mouseenter', () => {
-//     const existingAddRemoveButton = document.getElementById(ADD_REMOVE_BUTTON_ID);
+    #${BUTTON_ID}:hover {
+      width: min-content;
+    }
 
-//     if (existingAddRemoveButton) {
-//       document.body.removeChild(existingAddRemoveButton);
-//     }
+    #${LABEL_ID} {
+      margin-right: 3px;
+      white-space: nowrap;
+      opacity: 0;
+      visibility: hidden;
+    }
 
-//     const rect = script.getBoundingClientRect();
-//     const button = document.createElement("button");
+    #${BUTTON_ID}:hover #${LABEL_ID} {
+      opacity: 1;
+      visibility: visible;
+    }
+  `;
 
-//     button.id = ADD_REMOVE_BUTTON_ID;
-//     button.style.border = "none";
-//     button.style.background = "#D86299"
-//     button.style.borderRadius = "8px";
-//     button.style.fontFamily = "sans-serif";
-//     button.style.padding = "2px 4px";
-//     button.style.margin = "0";
-//     button.style.color = "white";
-//     button.style.left = `${rect.x + rect.width}px`;
-//     button.style.top = `${rect.y - rect.height}px`;
-//     button.style.position = "fixed";
-//     button.textContent = "+ Add to Powerlet"
+  button.append(icon);
+  button.append(label);
 
-//     shadow.appendChild(button);
-//   });
-// });
+  shadow.appendChild(style);
+  shadow.appendChild(button);
+});
 
