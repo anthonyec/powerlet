384cd7fb5bdf80dcc97616d217948262edc8604f
Anthony Cossins <1451668+anthonyec@users.noreply.github.com>
2023-04-26 20:30:03 +0800

    Refactor creating and removing bookmarks into hook

---
+++ src/popup/hooks/use_browser_bookmarks.js
@@ -0,0 +1,101 @@
+import { useDispatch, useSelector } from 'react-redux';
+
+import { selectTranslations } from '../store/selectors/locale';
+import { selectRecents } from '../store/selectors/bookmarklets';
+import { addRecentBookmarklet } from '../store/actions/bookmarklets';
+import { useToast } from './useToast';
+import { useUndoHistory } from './useUndoHistory';
+
+export function useBrowserBookmarks() {
+  const dispatch = useDispatch();
+  const translations = useSelector(selectTranslations);
+  const recents = useSelector(selectRecents);
+  const undoHistory = useUndoHistory();
+  const toast = useToast();
+
+  const create = (options) => {
+    return new Promise((resolve) => {
+      chrome.bookmarks.create(options, (result) => {
+        // A handle to get a dynamic reference to the bookmark ID. The bookmark
+        // ID can potentially change between deleting bookmarks and undoes,
+        // causing a "Can't find bookmark for id" error and preventing undo
+        // working correctly.
+        //
+        // Without handles, the following scenario can happen because we can't
+        // control what ID is used for creating bookmarks.
+        // - User creates a bookmark, Chrome gives it the ID of `1`.
+        // - Push onto the undo stack: Delete bookmark with ID `1`.
+        // - User deletes bookmark with ID `1`
+        // - Push onto the undo stack: Create bookmark
+        // - User undoes
+        // - Pop the stack and create a bookmark, Chrome gives it the ID of `2`
+        // - User undoes
+        // - Pop the stack and try delete bookmark with ID `1`
+        // - Bookmark with that ID does not exist!
+        const handle = undoHistory.createHandle(result.id);
+
+        undoHistory.push(
+          () =>
+            new Promise((undoResolve) => {
+              chrome.bookmarks.remove(handle.value, () => {
+                undoHistory.removeHandle(handle);
+                undoResolve();
+              });
+            })
+        );
+
+        resolve(result);
+      });
+    });
+  };
+
+  const remove = (bookmark) => {
+    return new Promise((resolve) => {
+      chrome.bookmarks.remove(bookmark.id, () => {
+        const handle = undoHistory.getHandleByValue(bookmark.id);
+        const isRecent = recents.includes(bookmark.id);
+
+        undoHistory.push(
+          () =>
+            new Promise((undoResolve) => {
+              chrome.bookmarks.create(
+                {
+                  parentId: bookmark.parentId,
+                  index: bookmark.index,
+                  title: bookmark.title,
+                  url: bookmark.url
+                },
+                (result) => {
+                  if (handle) {
+                    // Update the old ID with the new ID so that when undoing
+                    // creation of bookmarks, the correct ID is always available.
+                    handle.update(result.id);
+                  }
+
+                  if (isRecent) {
+                    dispatch(addRecentBookmarklet(result.id));
+                  }
+
+                  toast.hide();
+                  undoResolve();
+                }
+              );
+            })
+        );
+
+        toast.show(
+          translations['script_deleted_toast'].replace('%s', bookmark.title),
+          'Undo',
+          undoHistory.pop
+        );
+
+        resolve();
+      });
+    });
+  };
+
+  return {
+    create,
+    remove
+  };
+}
--- src/popup/screens/edit_bookmarklet_screen/index.jsx
+++ src/popup/screens/edit_bookmarklet_screen/index.jsx
@@ -1,26 +1,22 @@
 import React, { useEffect, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
 
 import Button from '../../components/button';
 import TextField from '../../components/text_field';
 import Titlebar from '../../components/titlebar';
 import { selectTranslations } from '../../store/selectors/locale';
-import { selectRecents } from '../../store/selectors/bookmarklets';
-import { useUndoHistory } from '../../hooks/useUndoHistory';
+import { useBrowserBookmarks } from '../../hooks/use_browser_bookmarks';
 import { useToast } from '../../hooks/useToast';
 
 import './edit_bookmarklet_screen.css';
-import { addRecentBookmarklet } from '../../store/actions/bookmarklets';
 
 export default function EditBookmarkletScreen({
   route = { params: {}, base: '' }
 }) {
-  const dispatch = useDispatch();
   const [bookmarklet, setBookmarklet] = useState({});
-  const recents = useSelector(selectRecents);
   const translations = useSelector(selectTranslations);
-  const undoHistory = useUndoHistory();
   const toast = useToast();
+  const bookmarks = useBrowserBookmarks();
 
   useEffect(() => {
     toast.hide();
@@ -35,38 +31,15 @@ export default function EditBookmarkletScreen({
         url: 'javascript: '
       };
 
-      chrome.bookmarks.create(newBookmarklet, (result) => {
-        // A handle to get a dynamic reference to the bookmark ID. The bookmark
-        // ID can potentially change between deleting bookmarks and undoes,
-        // causing a "Can't find bookmark for id" error and preventing undo
-        // working correctly.
-        //
-        // Without handles, the following scenario can happen because we can't
-        // control what ID is used for creating bookmarks.
-        // - User creates a bookmark, Chrome gives it the ID of `1`.
-        // - Push onto the undo stack: Delete bookmark with ID `1`.
-        // - User deletes bookmark with ID `1`
-        // - Push onto the undo stack: Create bookmark
-        // - User undoes
-        // - Pop the stack and create a bookmark, Chrome gives it the ID of `2`
-        // - User undoes
-        // - Pop the stack and try delete bookmark with ID `1`
-        // - Bookmark with that ID does not exist!
-        const handle = undoHistory.createHandle(result.id);
-
-        undoHistory.push(
-          () =>
-            new Promise((resolve) => {
-              chrome.bookmarks.remove(handle.value, () => {
-                undoHistory.removeHandle(handle);
-                resolve();
-              });
-            })
-        );
-
-        window.location.hash = `edit/${result.id}`;
-      });
-      return;
+      bookmarks
+        .create(newBookmarklet)
+        .then((result) => {
+          window.location.hash = `edit/${result.id}`;
+        })
+        .catch((error) => {
+          console.error('Failed to create bookmark', error);
+          window.location.hash = ``;
+        });
     }
 
     const handleBookmarksChange = () => {
@@ -90,46 +63,9 @@ export default function EditBookmarkletScreen({
     };
   }, [route.params.id]);
 
-  const handleRemoveClick = () => {
-    chrome.bookmarks.remove(bookmarklet.id, () => {
-      const handle = undoHistory.getHandleByValue(bookmarklet.id);
-      const isRecent = recents.includes(bookmarklet.id);
-
-      undoHistory.push(
-        () =>
-          new Promise((resolve) => {
-            chrome.bookmarks.create(
-              {
-                parentId: bookmarklet.parentId,
-                index: bookmarklet.index,
-                title: bookmarklet.title,
-                url: bookmarklet.url
-              },
-              (result) => {
-                if (handle) {
-                  // Update the old ID with the new ID so that when undoing
-                  // creation of bookmarks, the correct ID is always available.
-                  handle.update(result.id);
-                }
-
-                if (isRecent) {
-                  dispatch(addRecentBookmarklet(result.id));
-                }
-
-                toast.hide();
-                resolve();
-              }
-            );
-          })
-      );
-
-      toast.show(
-        translations['script_deleted_toast'].replace('%s', bookmarklet.title),
-        'Undo',
-        undoHistory.pop
-      );
-      window.location.hash = '';
-    });
+  const handleRemoveClick = async () => {
+    await bookmarks.remove(bookmarklet);
+    window.location.hash = '';
   };
 
   const handleBackClick = () => {
