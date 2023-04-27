3880775bc43223a00575366266313ca39c6040c6
Anthony Cossins <1451668+anthonyec@users.noreply.github.com>
2023-04-27 15:27:09 +0800

    Update bookmarks when moved into folders

--- src/popup/screens/home_screen/index.jsx
+++ src/popup/screens/home_screen/index.jsx
@@ -69,11 +69,13 @@ export default function HomeScreen() {
     handleBookmarksChange();
     chrome.bookmarks.onChanged.addListener(handleBookmarksChange);
     chrome.bookmarks.onCreated.addListener(handleBookmarksChange);
+    chrome.bookmarks.onMoved.addListener(handleBookmarksChange);
     chrome.bookmarks.onRemoved.addListener(handleBookmarksChange);
 
     return () => {
       chrome.bookmarks.onChanged.removeListener(handleBookmarksChange);
       chrome.bookmarks.onCreated.removeListener(handleBookmarksChange);
+      chrome.bookmarks.onMoved.removeListener(handleBookmarksChange);
       chrome.bookmarks.onRemoved.removeListener(handleBookmarksChange);
     };
   }, []);
