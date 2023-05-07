caa5e2aab089914ed93accc13d9b2b2fe0ff60ca
Anthony Cossins <1451668+anthonyec@users.noreply.github.com>
2023-05-07 18:28:45 +0100

    Remove drag to toggle

--- src/popup/components/toggle/index.jsx
+++ src/popup/components/toggle/index.jsx
@@ -1,61 +1,34 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 
 import './toggle.css';
 
 export default function Toggle({ onChange = () => {}, checked }) {
   const [isChecked, setIsChecked] = useState(checked);
-  const [mouseDownPosition, setMouseDownPosition] = useState(null);
-  const [isClicked, setIsClicked] = useState(false);
-
-  useEffect(() => {
-    const handleMouseMove = (event) => {
-      if (mouseDownPosition !== null) {
-        const x = event.clientX;
-        const difference = Math.abs(mouseDownPosition.x - x);
-
-        setIsChecked(difference > 10 && x > mouseDownPosition.x);
-      }
-    };
-
-    const handleMouseUp = () => {
-      setMouseDownPosition(null);
-    };
-
-    window.addEventListener('mousemove', handleMouseMove);
-    window.addEventListener('mouseup', handleMouseUp);
-
-    return () => {
-      window.removeEventListener('mousemove', handleMouseMove);
-      window.removeEventListener('mouseup', handleMouseUp);
-    };
-  }, [mouseDownPosition]);
+  const [disableFocusStyles, setDisableFocusStyles] = useState(false);
 
   const handleCheckboxClicked = () => {
     setIsChecked(!isChecked);
   };
 
   const handleCheckboxMouseDown = (event) => {
-    setIsClicked(true);
-    setMouseDownPosition({
-      x: event.clientX,
-      y: event.clientY
-    });
+    setDisableFocusStyles(true);
   };
 
   const handleCheckboxMouseUp = () => {
-    setIsClicked(true);
+    setDisableFocusStyles(true);
   };
 
   const handleCheckboxFocus = () => {
-    setIsClicked(false);
+    setDisableFocusStyles(false);
   };
 
   const handleCheckboxChange = (event) => {
-    const value = event.target.checked;
-    onChange(value);
+    onChange(event.target.checked);
   };
 
-  const toggleClassName = isClicked ? 'toggle toggle--clicked' : 'toggle';
+  const toggleClassName = disableFocusStyles
+    ? 'toggle toggle--disable-focus-styles'
+    : 'toggle';
 
   return (
     <div className={toggleClassName}>
